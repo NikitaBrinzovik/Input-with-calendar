85cd4abca9c84f8b11afe7685070ee0455d3368a
Nikita Kuznetsov <75593249+NikitaBrinzovik@users.noreply.github.com>
2021-11-13 02:52:52 +0300

    init: Code style feat: can past, can switch to calendar and back fix: remove garbage to-fix: remove const from component

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -3,6 +3,25 @@ import {parseDate} from "../helpers/parseDate";
 import {getDate} from "../helpers/getDate";
 import {KeyboardKey} from "../enums/keyboardKey";
 
+const ZERO = "0"
+const DOUBLE_ZERO = "00"
+const ZERO_ONE = "01"
+const ZERO_TWO = "02"
+const ZERO_THREE = "03"
+const ZERO_TEN = "010"
+const ZERO_ELEVEN = "011"
+const ZERO_TWELVE = "012"
+const ZERO_THIRTEEN = "013"
+const TEN = "10"
+const ELEVEN = "11"
+const TWELVE = "12"
+const TWENTY_THREE = "23"
+const TWENTY_SEVEN = "27"
+const TWENTY_EIGHT = "28"
+const THIRTY = "30"
+const THIRTY_ONE = "31"
+const FIFTY_NINE = "59"
+
 export const Calendar = () => {
     const [inputType, setInputType] = useState<string>("");
     const [inputValue, setInputValue] = useState<Date | string>("");
@@ -10,26 +29,6 @@ export const Calendar = () => {
     const {
         day, month, year, hours, minutes, seconds
     } = getDate(inputValue)
-
-    const ZERO = "0"
-    const DOUBLE_ZERO = "00"
-    const ZERO_ONE = "01"
-    const ZERO_TWO = "02"
-    const ZERO_THREE = "03"
-    const ZERO_TEN = "010"
-    const ZERO_ELEVEN = "011"
-    const ZERO_TWELVE = "012"
-    const ZERO_THIRTEEN = "013"
-    const TEN = "10"
-    const ELEVEN = "11"
-    const TWELVE = "12"
-    const TWENTY_THREE = "23"
-    const TWENTY_SEVEN = "27"
-    const TWENTY_EIGHT = "28"
-    const THIRTY = "30"
-    const THIRTY_ONE = "31"
-    const FIFTY_NINE = "59"
-
 
     const updatedDay = (+day + 1)
     const updatedYear = (+year + 1)
@@ -40,33 +39,32 @@ export const Calendar = () => {
     const updatedNegativeDay = (+day - 1)
     const updatedNegativeMonth = ZERO + (+month - 1)
     const updatedNegativeYear = (+year - 1)
-    const leapYear = ((year % 100 !== 0 && year % 4 === 0) || (year % 100 === 0 && year % 400 === 0))
 
+    const leapYear = ((year % 100 !== 0 && year % 4 === 0) || (year % 100 === 0 && year % 400 === 0))
     const notLeapYear = !((year % 100 !== 0 && year % 4 === 0) || (year % 100 === 0 && year % 400 === 0))
+    const firstOfMarch = month === 3 && day === 1
+    const firstOfFebruary = month === 2 && day === 1
     const february = month === 2 && day > 26
     const februaryInLeapYear = day > 27
-    const monthWithThirtyDays = day > 29 && (month === 4 || month === 6 || month === 9 || month === 11)
     const monthWithThirtyOneDays = day > 30
+    const monthWithThirtyDays = day > 29 && (month === 4 || month === 6 || month === 9 || month === 11)
+    const beforeMonthWithThirtyDays = day === 1 && (month === 5 || month === 7 || month === 10 || month === 12)
+    const firstDayOfMonthWithThirtyDays = day === 1 && (month === 4 || month === 6 || month === 9 || month === 11)
     const endOfDay = hours === 23
     const endOfHour = minutes === 59
     const endOfMinute = seconds === 59
     const endOfYear = updatedMonth === ZERO_THIRTEEN
-
-    const firstOfMarch = month === 3 && day === 1
-    const beforeMonthWithThirtyDays = day === 1 && (month === 5 || month === 7 || month === 10 || month === 12)
+    const previousYear = updatedNegativeMonth === DOUBLE_ZERO
     const firstDay = day === 1
     const zeroHour = hours === 0
     const zeroMinutes = minutes === 0
     const zeroSeconds = seconds === 0
-    const previousYear = updatedNegativeMonth === DOUBLE_ZERO
-
-    const firstOfFebruary = month === 2 && day === 1
-    const firstDayOfMonthWithThirtyDays = day === 1 && (month === 4 || month === 6 || month === 9 || month === 11)
 
 
     const handlePressArrowUpAndCtrl = () => {
 
         if (february) {
+
             if (notLeapYear) {
                 return setInputValue(parseDate(`${ZERO_ONE} ${ZERO_THREE} ${year} ${hours} ${minutes} ${seconds}`))
             }
@@ -90,12 +88,11 @@ export const Calendar = () => {
         }
 
         if (monthWithThirtyOneDays) {
+
             if (updatedMonth === ZERO_ELEVEN) {
                 return setInputValue(parseDate(`${ZERO_ONE} ${ELEVEN} ${year} ${hours} ${minutes} ${seconds}`))
-            } else {
-                return setInputValue(parseDate(`${ZERO_ONE} ${updatedMonth} ${year} ${hours} ${minutes} ${seconds}`))
-
             }
+                return setInputValue(parseDate(`${ZERO_ONE} ${updatedMonth} ${year} ${hours} ${minutes} ${seconds}`))
 
         }
 
@@ -104,8 +101,10 @@ export const Calendar = () => {
         }
 
         if (endOfHour) {
+
             if (endOfDay) {
-                return setInputValue(parseDate(`${updatedDay} ${month} ${year} ${DOUBLE_ZERO} ${DOUBLE_ZERO} ${seconds}`))
+                return setInputValue(
+                    parseDate(`${updatedDay} ${month} ${year} ${DOUBLE_ZERO} ${DOUBLE_ZERO} ${seconds}`))
             }
 
             return setInputValue(parseDate(`${day} ${month} ${year} ${updatedHours} ${DOUBLE_ZERO} ${seconds}`))
@@ -116,12 +115,13 @@ export const Calendar = () => {
         }
 
         if (endOfYear) {
-            return setInputValue(parseDate(`${day} ${ZERO_ONE} ${updatedYear} ${hours} ${minutes} ${seconds}`))
+            return setInputValue(parseDate(`${ZERO_ONE} ${ZERO_ONE} ${updatedYear} ${hours} ${minutes} ${seconds}`))
         }
     }
 
     const handlePressArrowUp = () => {
         if (february) {
+
             if (notLeapYear) {
                 return setInputValue(parseDate(`${ZERO_ONE} ${month} ${year} ${hours} ${minutes} ${seconds}`))
             }
@@ -130,6 +130,7 @@ export const Calendar = () => {
                 return setInputValue(parseDate(`${ZERO_ONE} ${month} ${year} ${hours} ${minutes} ${seconds}`))
             }
         }
+
         if (monthWithThirtyDays) {
             return setInputValue(parseDate(`${ZERO_ONE} ${month} ${year} ${hours} ${minutes} ${seconds}`))
         }
@@ -138,10 +139,9 @@ export const Calendar = () => {
 
 
     const handlePressArrowDownCtrlKey = () => {
-        // проверить февраль
-        // magic in firstDay
 
         if (firstOfMarch) {
+
             if (leapYear) {
                 return setInputValue(parseDate(`${TWENTY_EIGHT} ${ZERO_TWO} ${year} ${hours} ${minutes} ${seconds}`))
             }
@@ -149,27 +149,40 @@ export const Calendar = () => {
         }
 
         if (beforeMonthWithThirtyDays) {
+
             if (updatedNegativeMonth === ZERO_ELEVEN) {
                 return setInputValue(parseDate(`${THIRTY} ${ELEVEN} ${year} ${hours} ${minutes} ${seconds}`))
             }
 
             return setInputValue(parseDate(`${THIRTY} ${updatedNegativeMonth} ${year} ${hours} ${minutes} ${seconds}`))
         }
+
         if (firstDay) {
+
             if (updatedNegativeMonth === ZERO_TEN) {
 
                 return setInputValue(parseDate(`${THIRTY_ONE} ${TEN} ${year} ${hours} ${minutes} ${seconds}`))
             }
+
             if (updatedNegativeMonth === DOUBLE_ZERO) {
-                return setInputValue(parseDate(`${THIRTY_ONE} ${TWELVE} ${updatedNegativeYear} ${hours} ${minutes} ${seconds}`))
+                return setInputValue(
+                    parseDate(`${THIRTY_ONE} ${TWELVE} ${updatedNegativeYear} ${hours} ${minutes} ${seconds}`))
             }
+
             return setInputValue(
                 parseDate(`${THIRTY_ONE} ${updatedNegativeMonth} ${year} ${hours} ${minutes} ${seconds}`))
         }
 
         if (zeroHour) {
-            return setInputValue(parseDate(`${updatedNegativeDay} ${month} ${year} ${TWENTY_THREE} ${minutes} ${seconds}`))
+            return setInputValue(
+                parseDate(`${updatedNegativeDay} ${month} ${year} ${TWENTY_THREE} ${minutes} ${seconds}`))
         }
+
+        if (previousYear) {
+            return setInputValue(
+                parseDate(`${THIRTY_ONE} ${TWELVE} ${updatedNegativeYear} ${hours} ${minutes} ${seconds}`))
+        }
+
         if (zeroMinutes) {
             if (zeroHour) {
                 return setInputValue(
@@ -178,30 +191,26 @@ export const Calendar = () => {
             return setInputValue(
                 parseDate(`${day} ${month} ${year} ${updatedNegativeHours} ${FIFTY_NINE} ${seconds}`))
         }
-        if (zeroSeconds) {
-            return setInputValue(
-                parseDate(`${day} ${month} ${year} ${hours} ${minutes} ${FIFTY_NINE}`))
-        }
 
-        if (previousYear) {
-            return setInputValue(
-                parseDate(`${day} ${TWELVE} ${updatedNegativeYear} ${hours} ${minutes} ${seconds}`))
+        if (zeroSeconds) {
+            return setInputValue(parseDate(`${day} ${month} ${year} ${hours} ${minutes} ${FIFTY_NINE}`))
         }
 
     }
     const handlePressArrowDown = () => {
         if (firstOfFebruary) {
+
             if (leapYear) {
                 return setInputValue(parseDate(`${TWENTY_EIGHT} ${ZERO_TWO} ${year} ${hours} ${minutes} ${seconds}`))
             }
+
             if (firstDay) {
                 return setInputValue(parseDate(`${TWENTY_SEVEN} ${ZERO_TWO} ${year} ${hours} ${minutes} ${seconds}`))
             }
         }
 
         if (firstDayOfMonthWithThirtyDays) {
-            return setInputValue(
-                parseDate(`${THIRTY} ${month} ${year} ${hours} ${minutes} ${seconds}`))
+            return setInputValue(parseDate(`${THIRTY} ${month} ${year} ${hours} ${minutes} ${seconds}`))
         }
     }
 
@@ -233,8 +242,8 @@ export const Calendar = () => {
 
     }
     const handleResetInput = () => {
-        setInputType('')
-        setInputValue('')
+        setInputType("")
+        setInputValue("")
     }
 
     const handlePressEnter = () => {
@@ -246,11 +255,11 @@ export const Calendar = () => {
         <div>
             <input
                 type={inputType}
-                className={'calendar'}
-                placeholder={'hey'}
+                className={"calendar"}
+                placeholder={"hey"}
                 value={inputValue.toString()}
                 onKeyDown={onKeyDown}
-                id={'calendar'}
+                id={"calendar"}
                 onChange={(e) => setInputValue(e.currentTarget.value)}
             />
         </div>
