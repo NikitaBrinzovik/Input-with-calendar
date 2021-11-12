e662d1151941d2efd3199f47c48e1629b070ab7f
Nikita Kuznetsov <75593249+NikitaBrinzovik@users.noreply.github.com>
2021-11-13 02:08:37 +0300

    Delete src/types directory

--- src/types/index.ts
+++
@@ -1,8 +0,0 @@
-export type ArrowvalidationType = {
-    dayFromDate: number |any
-    monthFromDate: number |any
-    yearFromDate: number|any
-    hoursFromDate: number|any
-    minutesFromDate: number|any
-    secondsFromDate: number|any
-}
